5180237c59c75fe4fe8acab1fdbe809802e01023
danvaan <danvaan@users.noreply.github.com>
2017-11-13 11:23:11 +0200

    polypoly

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+
+            Random rnd = new Random();
+            int randomNumber = rnd.Next(1, 101);
+            int arv1 = 0;
+
+            Console.WriteLine("Arva ära suvaline number");
+            Console.WriteLine();
+            Console.WriteLine("Kirjuta number 1-100");
+            Console.WriteLine();
+           
+            while (arv1 != randomNumber)
+            {
+
+                arv1 = int.Parse(Console.ReadLine());
+
+                if (arv1 > randomNumber)
+                {
+
+                    Console.WriteLine("sinu pakutud arv on suurem, arva uuesti");
+                    continue;
+                }
+                if (arv1 < randomNumber)
+                {
+                    Console.WriteLine("sinu pakutud arv on väiksem, arva uuesti");
+                    continue;
+                }
+                if (arv1 == randomNumber)
+                {
+                    break;
+                }
+
+
+
+
+
+              
+
+
+
+            }
+            Console.WriteLine("arvasid õigesti");
+
+            Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
